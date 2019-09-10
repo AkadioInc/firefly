5747671146791bbeb53e105e37897a6346ca9bb2
John Readey <jreadey@hdfgroup.org>
2019-09-10 16:41:35 +0200

    show datetimee rather than timestamp in dump

--- k8s_apps/admin/dump_inventory_file.py
+++ k8s_apps/admin/dump_inventory_file.py
@@ -1,11 +1,28 @@
 import h5pyd
+from datetime import datetime
+import tzlocal
+
 BUCKET="firefly-hsds"
 inventory_domain = "/FIREfly/inventory.h5"
+
+def formatTime(timestamp):
+    local_timezone = tzlocal.get_localzone() # get pytz timezone
+    local_time = datetime.fromtimestamp(timestamp, local_timezone)
+    return local_time
+
+
+
 f = h5pyd.File(inventory_domain, "r", bucket=BUCKET)
 table = f["inventory"]
 for row in table:
     filename = row[0].decode('utf-8')
-    start = row[1]
-    stop = row[2]
+    if row[1]:
+        start = formatTime(row[1])
+    else:
+        start = 0
+    if row[2]:
+        stop = formatTime(row[2])
+    else:
+        stop = 0
     print(f"{filename}\t{start}\t{stop}")
 print(f"{table.nrows} rows")
